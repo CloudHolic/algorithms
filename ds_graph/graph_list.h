15a30c1da3b2490c9f08b299a6857e58e766d06d
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-04-15 15:14:19 +0900

    declare graph header file

--- ds_graph/graph_list.h
+++ ds_graph/graph_list.h
@@ -5,10 +5,33 @@
 
 using namespace std;
 
-class graph_matrix
+struct graph_edge
+{
+    int start_vertex, end_vertex;
+    int weight;
+};
+
+class graph_list
 {
 public:
+    // Constructor
+    graph_list(int size, graph_edge edges[]);
+
+    bool is_weighted() const;
+    bool is_directed() const;
+    bool is_bipartite() const;
+    bool is_planar() const;
+
 private:
+    struct node
+    {
+        int vertex, weight;
+        node* next_node;
+    };
+
     int size_;
-    vector<vector<int>> internal_list_;
+    bool weighted_;
+    bool directed_;
+
+    vector<node*> internal_list_;
 };
--- ds_graph/graph_matrix.h
+++ ds_graph/graph_matrix.h
@@ -5,10 +5,27 @@
 
 using namespace std;
 
+struct graph_edge
+{
+    int start_vertex, end_vertex;
+    int weight;
+};
+
 class graph_matrix
 {
 public:
+    // Constructor
+    graph_matrix(int size, graph_edge edges[]);
+
+    bool is_weighted() const;
+    bool is_directed() const;
+    bool is_bipartite() const;
+    bool is_planar() const;
+
 private:
     int size_;
+    bool weighted_;
+    bool directed_;
+
     vector<vector<int>> internal_matrix_;
 };
