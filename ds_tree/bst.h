7de8abf38c502cdd1fdf32d917e4860a86ac3b8f
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-04-21 17:29:45 +0900

    Implement some functions in bst

---
+++ ds_tree/bst.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+class bst
+{
+public:
+    // Constructor
+    bst(int size);
+
+    // Destructor
+    ~bst();
+
+    // Operator overloading
+    int operator[](const int index);
+
+    void insert(const int value);
+    void remove(const int value);
+    int find_index(const int value) const;
+
+    void clear();
+
+private:
+    vector<int*> nodes_;
+    int max_size_, current_size_;
+};
