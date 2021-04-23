12d1e3efa6211e9f385a8037fb8b9b4867b2b623
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-04-23 16:56:01 +0900

    declare rbt

--- ds/tree/rbt.cpp
+++ ds/tree/rbt.cpp
@@ -1,1 +1,101 @@
 #include "rbt.h"
+
+rbt::rbt() : root_(nullptr) {}
+
+rbt:: rbt(const rbt& prev) : root_(nullptr)
+{
+
+}
+
+rbt::rbt(rbt&& prev) noexcept : root_(nullptr)
+{
+    *this = std::move(prev);
+}
+
+rbt::~rbt()
+{
+    internal_clear(root_);
+}
+
+rbt& rbt::operator=(const rbt& prev)
+{
+    if(this != &prev)
+    {
+        clear();
+    }
+
+    return *this;
+}
+
+rbt& rbt::operator=(rbt&& prev) noexcept
+{
+    if(this != &prev)
+    {
+        clear();
+
+        std::swap(root_, prev.root_);
+
+        if(root_->left != nullptr)
+            root_->left->parent = root_;
+        if(root_->right != nullptr)
+            root_->right->parent = root_;
+    }
+
+    return *this;
+}
+
+void rbt::insert(const int value)
+{
+
+}
+
+void rbt::remove(const int value)
+{
+
+}
+
+bool rbt::exist(const int value) const
+{
+
+}
+
+vector<int> rbt::inorder() const
+{
+
+}
+
+vector<int> rbt::levelorder() const
+{
+
+}
+
+void rbt::clear()
+{
+    internal_clear(root_);
+}
+
+rbt::node::node(int value) : data(value)
+{
+    left = right = parent = nullptr;
+    color = RED;
+}
+
+void rbt::internal_clear(node* root)
+{
+    if(root->left != nullptr)
+        internal_clear(root->left);
+    if(root->right != nullptr)
+        internal_clear(root->right);
+
+    if(root->parent != nullptr)
+    {
+        if(root->parent->left == root)
+            root->parent->left = nullptr;
+        else if(root->parent->right == root)
+            root->parent->right = nullptr;
+
+        root->parent = nullptr;
+    }
+
+    delete root;
+}
--- ds/tree/rbt.h
+++ ds/tree/rbt.h
@@ -1,6 +1,48 @@
 #pragma once
 
+#include <vector>
+
+using namespace std;
 class rbt
 {
+public:
+    // Constructors
+    rbt();
+    rbt(const rbt& prev);
+    rbt(rbt&& prev) noexcept;
 
+    // Destructor
+    ~rbt();
+
+    // Operator overloading
+    rbt& operator=(const rbt& prev);
+    rbt& operator=(rbt&& prev) noexcept;
+
+    // Other methods
+    void insert(const int value);
+    void remove(const int value);
+    bool exist(const int value) const;
+    vector<int> inorder() const;
+    vector<int> levelorder() const;
+    void clear();
+
+private:
+    enum node_color
+    {
+        RED,
+        BLACK
+    };
+    struct node
+    {
+        int data;
+        node_color color;
+        node *left, *right, *parent;
+
+        node(int value);
+    };
+
+    node* root_;
+
+    // Private methods
+    void internal_clear(node* root);
 };
