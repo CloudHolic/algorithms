7df552668a12fb98dbceebb0ae547abc16b4ab59
이한석 <bluegkstjr@kaist.ac.kr>
2021-11-30 11:10:50 +0900

    Update FFT

--- Algorithm/Math/FFT.fsx
+++ Algorithm/Math/FFT.fsx
@@ -1,92 +1,50 @@
+open System
+open System.Numerics
+
 module FastFourierTransform =
-    open System
-    open System.Numerics
+    let tau = 2. * Math.PI
 
-    let maxSize = 262144
-    let pi = Math.PI
-    let tau = 2. * pi
-
-    module internal Details =
-        let isPowerOf2 n = (n &&& n - 1) = 0
-
-        let ilog2 n =
-            if n < 2 then failwith "n must be greater than 1"
-            if not (isPowerOf2 n) then failwith "n must be a power of 2"
-            let rec loop n c s =
-                let t = 1 <<< c
-                if t = n then c
-                elif t > n then
-                    loop n (c - s) (s >>> 1)
-                else
-                    loop n (c + s) (s >>> 1)
-            loop n 16 8
-
-        let root a inv =
-            if not inv then Complex.FromPolarCoordinates(1., a)
-            else Complex.FromPolarCoordinates(1., a) |> Complex.Conjugate
-
-        let roots inv =
-            let unfolder c =
-                if c < 2 * maxSize then
-                    let vs = Array.init (c / 2) (fun i -> root (tau * float i / float c) inv)
-                    Some (vs, c*2)
-                else
-                    None
-
-            Array.unfold unfolder 1
-
-        let rec loop n2 ln s c f t inv =
-            if c > 2 then
-                let c2 = c >>> 1
-                let struct (t, f) = loop n2 (ln - 1) (s <<< 1) c2 f t inv
-                let roots = (roots inv).[ln]
-
-                if s > 1 then
-                    for j = 0 to c2 - 1 do
-                        let w = roots.[j]
-                        let off = s * j
-                        let off2 = off <<< 1;
-                        for i = 0 to s - 1 do
-                            let e = Array.get f (i + off2 + 0)
-                            let o = Array.get f (i + off2 + s)
-                            let a = w * o
-                            Array.set t (i + off + 0) (e + a)
-                            Array.set t (i + off + n2) (e - a)
-                else
-                    for j = 0 to c2 - 1 do
-                        let w = roots.[j]
-                        let e = Array.get f (2 * j + 0)
-                        let o = Array.get f (2 * j + s)
-                        let a = w * o
-                        Array.set t (j + 0)   (e + a)
-                        Array.set t (j + n2)  (e - a)
-
-                struct (f, t)
-
-            elif c = 2 then
-                for i = 0 to s - 1 do
-                    let e = Array.get f (i + 0)
-                    let o = Array.get f (i + s)
-                    let a = o
-                    Array.set t (i + 0)   (e + a)
-                    Array.set t (i + n2)  (e - a)
-
-                struct (f, t)
-
-            else
-                struct (t, f)
-
-    open Details
+    let root a inv =
+        if not inv then Complex.FromPolarCoordinates(1., a)
+        else Complex.FromPolarCoordinates(1., a) |> Complex.Conjugate
 
     let fft (vs : Complex []) invert : Complex [] =
         let n = vs.Length
-        let ln = ilog2 n
 
-        let vs0 = Array.copy vs
-        let vs1 = Array.zeroCreate n
+        let mutable result = Array.copy vs
 
-        let struct (_, t) = Details.loop (n >>> 1) ln 1 n vs0 vs1 invert
-        if invert then t |> Array.map (fun i -> i / Complex(float n, 0.)) else t
+        let rec loop ln inv =
+            if ln <= n then
+                let roots = root (tau / float ln) inv
+
+                for i in 0 .. ln .. n - 1 do
+                    let mutable w = Complex(1., 0.)
+                    for j = 0 to ln / 2 - 1 do
+                        let u = result.[i + j]
+                        let v = result.[i + j + ln / 2] * w
+
+                        result.[i + j] <- u + v
+                        result.[i + j + ln / 2] <- u - v
+
+                        w <- w * roots
+
+                loop (ln <<< 1) inv
+        
+        let mutable j = 0
+        for i = 1 to n - 1 do
+            let mutable bit = n >>> 1
+            while j >= bit do
+                j <- j - bit
+                bit <- bit >>> 1
+            j <- j + bit
+
+            if i < j then
+                let temp = result.[i]
+                result.[i] <- result.[j]
+                result.[j] <- temp
+
+        loop 2 invert
+        if invert then result |> Array.map (fun i -> i / Complex(float n, 0.)) else result
 
     let multiply arr1 arr2 =
         let size = Array.length arr1 + Array.length arr2 - 1
--- Algorithm/Math/Slow_FFT.fsx
+++ Algorithm/Math/Slow_FFT.fsx
@@ -1,42 +1,44 @@
 open System
 open System.Numerics
 
-let tau = 2. * Math.PI
+// Very naive and slow implementation of FFT.
+module FastFourierTransform =
+    let tau = 2. * Math.PI
 
-let root a inv = 
-    if not inv then Complex.FromPolarCoordinates(1., a)
-    else Complex.FromPolarCoordinates(1., a) |> Complex.Conjugate
+    let root a inv = 
+        if not inv then Complex.FromPolarCoordinates(1., a)
+        else Complex.FromPolarCoordinates(1., a) |> Complex.Conjugate
 
-let fft x invert =
-    let rec inner x invert =
-        match x, invert with
-        | [||], _ -> [||]
-        | [|x|], _ -> [|x|]
-        | x, invert ->
-            x
-            |> Array.mapi (fun i c -> i % 2 = 0, c)
-            |> Array.partition fst
-            |> fun (even, odd) -> inner (Array.map snd even) invert, inner (Array.map snd odd) invert
-            ||> Array.mapi2 (fun i even odd ->
-                let btf = odd * (root (tau * (float i / float x.Length)) invert)
-                even + btf, even - btf)
-            |> Array.unzip
-            ||> Array.append
+    let fft x invert =
+        let rec inner x invert =
+            match x, invert with
+            | [||], _ -> [||]
+            | [|x|], _ -> [|x|]
+            | x, invert ->
+                x
+                |> Array.mapi (fun i c -> i % 2 = 0, c)
+                |> Array.partition fst
+                |> fun (even, odd) -> inner (Array.map snd even) invert, inner (Array.map snd odd) invert
+                ||> Array.mapi2 (fun i even odd ->
+                    let btf = odd * (root (tau * (float i / float x.Length)) invert)
+                    even + btf, even - btf)
+                |> Array.unzip
+                ||> Array.append
 
-    inner x invert
-    |> function
-        | list ->
-            if invert then list |> Array.map (fun i -> i / Complex(float x.Length, 0.))
-            else list
+        inner x invert
+        |> function
+            | list ->
+                if invert then list |> Array.map (fun i -> i / Complex(float x.Length, 0.))
+                else list
 
-let multiply arr1 arr2 =
-    let size = Array.length arr1 + Array.length arr2 - 1
-    let size = Math.Pow(2., (Math.Log (float size) / Math.Log 2.) |> Math.Ceiling) |> int
+    let multiply arr1 arr2 =
+        let size = Array.length arr1 + Array.length arr2 - 1
+        let size = Math.Pow(2., (Math.Log (float size) / Math.Log 2.) |> Math.Ceiling) |> int
 
-    let coeff1 = Array.init size (fun i -> if i < Array.length arr1 then Complex(float arr1.[i], 0.) else Complex(0., 0.))
-    let coeff2 = Array.init size (fun i -> if i < Array.length arr2 then Complex(float arr2.[i], 0.) else Complex(0., 0.))
+        let coeff1 = Array.init size (fun i -> if i < Array.length arr1 then Complex(float arr1.[i], 0.) else Complex(0., 0.))
+        let coeff2 = Array.init size (fun i -> if i < Array.length arr2 then Complex(float arr2.[i], 0.) else Complex(0., 0.))
 
-    (fft coeff1 false, fft coeff2 false)
-    ||> Array.map2 (fun x y -> x * y)
-    |> function
-        | res -> fft res true |> Array.map (fun x -> Math.Floor (x.Real + 0.5) |> int)
+        (fft coeff1 false, fft coeff2 false)
+        ||> Array.map2 (fun x y -> x * y)
+        |> function
+            | res -> fft res true |> Array.map (fun x -> Math.Floor (x.Real + 0.5) |> int)
